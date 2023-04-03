03d0691ce4496d1c21a2e5f933667322dfa3e4d1
euanmillar <euanmillar77@gmail.com>
2023-04-03 16:23:44 +0100

    Update secrets paths

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,25 +1,18 @@
-import * as fs from 'fs'
-import * as util from 'util'
-
-const get = (secret: string) => {
-  try {
-    // Swarm secret are accessible within tmpfs /run/secrets dir
-    return fs
-      .readFileSync(util.format('/run/secrets/%s', secret), 'utf8')
-      .trim()
-  } catch (e) {
-    return false
-  }
-}
+import { readFileSync } from 'fs'
 
 export const HOST = process.env.HOST || '0.0.0.0'
 export const PORT = process.env.PORT || 4545
 export const WEBHOOK_URL = process.env.WEBHOOK_URL || '' // Insert your webhook URL
 export const AUTH_URL = process.env.AUTH_URL || '' // Insert the URL to your OpenCRVS auth service installation
 export const CALLBACK_URL = process.env.CALLBACK_URL || '' // Insert your webhooks URL here for Verification Request and Event Notification
-export const CLIENT_ID = get('CLIENT_ID') || (process.env.CLIENT_ID as string)
-export const CLIENT_SECRET =
-  get('CLIENT_SECRET') || (process.env.CLIENT_SECRET as string)
-export const SHA_SECRET =
-  get('SHA_SECRET') || (process.env.SHA_SECRET as string)
+
 export const DEFAULT_TIMEOUT = 600000
+export const SHA_SECRET = process.env.SHA_SECRET_PATH
+  ? readFileSync(process.env.SHA_SECRET_PATH).toString()
+  : ''
+export const CLIENT_SECRET = process.env.CLIENT_SECRET_PATH
+  ? readFileSync(process.env.CLIENT_SECRET_PATH).toString()
+  : ''
+export const CLIENT_ID = process.env.CLIENT_ID_PATH
+  ? readFileSync(process.env.CLIENT_ID_PATH).toString()
+  : ''
